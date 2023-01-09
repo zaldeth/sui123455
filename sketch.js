31def40dd447d5b6f8225ed74da84e69a49ad2c8
zaldeth <113926156+zaldeth@users.noreply.github.com>
2023-01-09 19:37:01 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,238 +1,61 @@
 const Engine = Matter.Engine;
+const Render = Matter.Render;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
 
-var engine, world;
-var canvas;
-var palyer, playerBase, playerArcher;
-var computer, computerBase, computerArcher;
-var playerArrows = [];
-var computerArrows = [];
-var playerArcherLife = 3;
-var computerArcherLife = 3;
-var computerCollision
-function preload() {
-  backgroundImg = loadImage("./assets/background.gif");
-}
+let engine;
+let world;
+var ball;
+var blower;
+var blowerMouth;
+var button;
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+  var canvas = createCanvas(500, 500);
 
   engine = Engine.create();
   world = engine.world;
 
-  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
-  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
-  playerArcher = new PlayerArcher(
-    340,
-    playerBase.body.position.y - 180,
-    120,
-    120
-  );
+  ball = new Ball(width / 2 + 80, height / 2 - 80, 80, 80);
+  blower = new Blower(width / 2 - 50, height / 2 + 50, 150, 20);
+  blowerMouth = new BlowerMouth(width / 2 + 70, height / 2 + 20, 100, 90);
+  button = createImg('click.png');
+button.position(20,30);
+button.size(50,50);
 
-  computerBase = new ComputerBase(
-    width - 300,
-    random(450, height - 300),
-    180,
-    150
-  );
-  computer = new Computer(
-    width - 280,
-    computerBase.body.position.y - 153,
-    50,
-    180
-  );
 
-  computerArcher = new ComputerArcher(
-    width - 350,
-    computerBase.body.position.y - 180,
-    120,
-    120
-  );
-  handleComputerArcher();
+// buttonmouseClicked(blow);
+
+ button.mouseClicked(blow);
+
+// button.Clicked(blow);
+
+// button.mouse(blow);
+
+
 }
 
 function draw() {
-  background(backgroundImg);
-
+  background(59);
   Engine.update(engine);
 
-  // Title
-  fill("#FFFF");
-  textAlign("center");
-  textSize(40);
-  text("EPIC ARCHERY", width / 2, 100);
-
-  for (var i = 0; i < playerArrows.length; i++) {
-    showArrows(i, playerArrows);
-  }
-
-  playerBase.display();
-  player.display();
-  player.life();
-  playerArcher.display();
-  handlePlayerArrowCollision();
-
-  for (var i = 0; i < computerArrows.length; i++) {
-    showArrows(i, computerArrows);
-  }
-
-  computerBase.display();
-  computer.display();
-  computer.life();
-  computerArcher.display();
-  handleComputerArrowCollision();
+  blower.show();
+  ball.show();
+  blowerMouth.show();
 }
 
-function keyPressed() {
-  if (keyCode === 32) {
-    var posX = playerArcher.body.position.x;
-    var posY = playerArcher.body.position.y;
-    var angle = playerArcher.body.angle;
+function blow() {
+  
+   //Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0, y: -0.05 });
+  
+  // Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0.05, y: 0.05 });
+  
+  Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0, y: 0.05 });
+  
+  // Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: -0.05, y: 0.05 });
 
-    var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
-
-    arrow.trajectory = [];
-    Matter.Body.setAngle(arrow.body, angle);
-    playerArrows.push(arrow);
-  }
-}
-
-function keyReleased() {
-  if (keyCode === 32) {
-    if (playerArrows.length) {
-      var angle = playerArcher.body.angle;
-      playerArrows[playerArrows.length - 1].shoot(angle);
-    }
-  }
-}
-
-function showArrows(index, arrows) {
-  arrows[index].display();
-  if (
-    arrows[index].body.position.x > width ||
-    arrows[index].body.position.y > height
-  ) {
-    if (!arrows[index].isRemoved) {
-      arrows[index].remove(index, arrows);
-    } else {
-      arrows[index].trajectory = [];
-    }
-  }
-}
-
-function handleComputerArcher() {
-  if (!computerArcher.collapse && !playerArcher.collapse) {
-    setTimeout(() => {
-      var pos = computerArcher.body.position;
-      var angle = computerArcher.body.angle;
-      var moves = ["UP", "DOWN"];
-      var move = random(moves);
-      var angleValue;
-
-      if (move === "UP" && computerArcher.body.angle < 1.87) {
-        angleValue = 0.1;
-      }else{
-          angleValue = -0.1;
-      }
-      if(move === "DOWN" && computerArcher.body.angle > 1.47) {
-        angleValue = -0.1;
-      }else{
-          angleValue = 0.1;
-      }
-      
-      angle += angleValue;
-
-      var arrow = new ComputerArrow(pos.x, pos.y, 100, 10, angle);
-
-      Matter.Body.setAngle(computerArcher.body, angle);
-      Matter.Body.setAngle(computerArcher.body, angle);
-
-      computerArrows.push(arrow);
-      setTimeout(() => {
-        computerArrows[computerArrows.length - 1].shoot(angle);
-      }, 100);
-
-      handleComputerArcher();
-    }, 2000);
-  }
-}
-
-function handlePlayerArrowCollision() {
-  for (var i = 0; i < playerArrows.length; i++) {
-    var baseCollision = Matter.SAT.collides(
-      playerArrows[i].body,
-      computerBase.body
-    );
-
-     var computerCollision = Matter.SAT.collides(
-      playerArrows[i].body,
-      computer.body
-    );
-
-    var computerArcherCollision = Matter.SAT.collides(
-      playerArrows[i].body,
-      computerArcher.body
-    );
-
-    if (
-      baseCollision.collided ||
-      computerArcherCollision.collided ||
-      computerCollision.collided
-    ) {
-
-      /**Update the code here so that computer life 
-      reduces if player's arrow hits the target***/
-      computerArcherLife -= 1;
-      computer.reduceLife(computerArcherLife);
-
-      if (computerArcherLife <= 0) {
-        computerArcher.collapse = true;
-        Matter.Body.setStatic(computerArcher.body, false);
-        Matter.Body.setStatic(computer.body, false);
-        Matter.Body.setPosition(computer.body, {
-          x: width - 100,
-          y: computer.body.position.y
-        });
-      }
-    }
-  }
-}
-
-function handleComputerArrowCollision() {
-  for (var i = 0; i < computerArrows.length; i++) {
-    var baseCollision = Matter.SAT.collides(
-      computerArrows[i].body,
-      playerBase.body
-    );
-
-    var playerCollision = Matter.SAT.collides(
-      computerArrows[i].body,
-      player.body
-    );
-
-    var playerArcherCollision = Matter.SAT.collides(
-      computerArrows[i].body,
-      playerArcher.body
-    );
-
-    if (
-      baseCollision.collided ||
-      playerCollision.collided||
-      playerArcherCollision.collided
-    ) {
-      playerArcherLife -= 1;
-      player.reduceLife(playerArcherLife);
-      if (playerArcherLife <= 0) {
-        playerArcher.collapse = true;
-        Matter.Body.setStatic(playerArcher.body, false);
-        Matter.Body.setStatic(player.body, false);
-        Matter.Body.setPosition(player.body, {
-          x: 100,
-          y: player.body.position.y
-        });
-      }
-    }
-  }
 }
